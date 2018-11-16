fb3a92f74acf2b54b0ad746028e26bff22b18d81
Vinicius Souza <viniciusls22@gmail.com>
2018-11-16 17:52:25 -0200

    Nova implementacao comunicacao SS
    Signed-off-by: Vinicius Souza <viniciusls22@gmail.com>

--- ss/receptor_sa.py
+++ ss/receptor_sa.py
@@ -8,12 +8,28 @@ class ReceptorSA(Thread):
 	"""Recebe e trata mensagens do SA"""
 
 	def __init__(self, host):
+		global shared_obj
 		super(ReceptorSA, self).__init__()
+		self._nome = shared_obj.get(SharedObj.NomeDoRobo)
+		self._exchange = 'SA_to_SA'
 
 		self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=str(host)))
 		self.channel = self.connection.channel()
-		self.channel.queue_declare(queue='SA_to_SS')
-		self.channel.basic_consume(self.trata_msg_recebida, queue='SA_to_SS', no_ack=True)
+		self.channel.exchange_declare(exchange=self._exchange, exchange_type='direct')
+
+		result = self.channel.queue_declare(exclusive=True)
+		self._queue_name = result.method.queue
+
+		self.channel.queue_bind(exchange=self._exchange, queue=self._queue_name, routing_key=self._nome)
+		self.channel.queue_bind(exchange=self._exchange, queue=self._queue_name, routing_key='cadastro')
+
+		self.channel.basic_consume(self.trata_msg_recebida, queue=self._queue_name, no_ack=True)
+
+
+	def _atualiza_nome(self, nome):
+		self._nome = nome
+		self.channel.queue_bind(exchange=self._exchange, queue=self._queue_name, routing_key=self._nome)
+
 
 	def trata_msg_recebida(self, ch, method, properties, body):
 		global shared_obj
@@ -23,10 +39,8 @@ class ReceptorSA(Thread):
 		except:
 			return
 
-		if 'robo' not in msg or \
-			(msg['robo'] != shared_obj.get(SharedObj.NomeDoRobo)
-			and msg['robo'] != 'cadastro'):
-			return
+		if method.routing_key == 'cadastro':
+			if 'nome' in msg: self._atualiza_nome(msg['nome'])
 
 		msg['_dir'] = 'sa'
 
